8d93fcd5a05ae8db554ff21d5e01d196ab8897bf
Tirthhalvadia <83959502+Tirthhalvadia@users.noreply.github.com>
2022-03-19 20:23:34 +0530

    first commit

---
+++ Health-checkup.py
@@ -0,0 +1,39 @@
+import urllib.request
+import json
+import os
+import ssl
+
+def allowSelfSignedHttps(allowed):
+    # bypass the server certificate verification on client side
+    if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
+        ssl._create_default_https_context = ssl._create_unverified_context
+
+allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+
+# Request data goes here
+data = {
+    "Inputs": {
+    },
+    "GlobalParameters": {
+    }
+}
+
+body = str.encode(json.dumps(data))
+
+url = 'http://4587bac2-5ec7-4a01-ba13-66a3521a2d4b.centralindia.azurecontainer.io/score'
+api_key = '79a43fb3-323f-4e90-9af8-52da8d081891' # Replace this with the API key for the web service
+headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+
+req = urllib.request.Request(url, body, headers)
+
+try:
+    response = urllib.request.urlopen(req)
+
+    result = response.read()
+    print(result)
+except urllib.error.HTTPError as error:
+    print("The request failed with status code: " + str(error.code))
+
+    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+    print(error.info())
+    print(error.read().decode("utf8", 'ignore'))
